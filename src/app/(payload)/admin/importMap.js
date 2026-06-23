bca41d18c7277376ddb44d040ef2475fa189f641
태연 <taeyeon@baegopayoui-MacBookAir.local>
2026-06-23 12:08:52 +0900

    feat: initial commit

---
+++ src/app/(payload)/admin/importMap.js
@@ -0,0 +1,14 @@
+import { FigmaAvatarCell as FigmaAvatarCell_5b632879807206965a35b58cd5166e1c } from '@payloadcms/figma/client'
+import { FigmaAvatar as FigmaAvatar_5b632879807206965a35b58cd5166e1c } from '@payloadcms/figma/client'
+import { DefaultLoginButton as DefaultLoginButton_5b632879807206965a35b58cd5166e1c } from '@payloadcms/figma/client'
+import { ContentApiClientUploadHandler as ContentApiClientUploadHandler_5b632879807206965a35b58cd5166e1c } from '@payloadcms/figma/client'
+import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
+
+/** @type import('payload').ImportMap */
+export const importMap = {
+  "@payloadcms/figma/client#FigmaAvatarCell": FigmaAvatarCell_5b632879807206965a35b58cd5166e1c,
+  "@payloadcms/figma/client#FigmaAvatar": FigmaAvatar_5b632879807206965a35b58cd5166e1c,
+  "@payloadcms/figma/client#DefaultLoginButton": DefaultLoginButton_5b632879807206965a35b58cd5166e1c,
+  "@payloadcms/figma/client#ContentApiClientUploadHandler": ContentApiClientUploadHandler_5b632879807206965a35b58cd5166e1c,
+  "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
+}
